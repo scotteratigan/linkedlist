c17ae3d52b23dc83d9f63df6033a604cba020225
Scott Ratigan <scotteratigan@gmail.com>
2020-03-22 13:47:34 -0700

    separating code

---
+++ main.js
@@ -0,0 +1,14 @@
+const myList = new LinkedList();
+myList.addLast(0);
+myList.addLast(1);
+myList.addLast(2);
+myList.addLast(3);
+const reduceFn = (accumulator, currentValue) => (accumulator += currentValue);
+const reduced = myList.reduce(reduceFn);
+console.log('reduced:', reduced);
+
+// const myClone = myList.clone();
+// console.log('myClone:', myClone);
+// myList.forEach(elm => console.log(elm));
+// console.log(JSON.stringify(myList));
+// console.log(JSON.stringify(myList.clone()));
